fe70f404804eca1581e8f8d7a463480c696c5132
佐藤貴之 <ta_sato@waku-2.com>
2015-08-04 23:41:36 +0900

    編集中

--- app/src/main/java/com/example/satoutakayuki/androidtraining/MainActivity.java
+++ app/src/main/java/com/example/satoutakayuki/androidtraining/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.satoutakayuki.androidtraining;
 
 import android.app.Activity;
-import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -23,8 +24,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-//        final int pgaeNum = 12;
-
+		// ダミーデータ作成
 		final List<String> imageUrls = new ArrayList<String>();
 		imageUrls.add("http://imgbp.hotp.jp/CSP/IMG_SRC/77/44/B002927744/B002927744_349-262.jpg");
 		imageUrls.add("http://imgbp.hotp.jp/CSP/IMG_SRC/77/34/B002927734/B002927734_349-262.jpg");
@@ -41,49 +41,131 @@ public class MainActivity extends Activity {
 
 		mPager = (ViewPager) findViewById(R.id.pager);
 		final MainPhotoViewPager adapter = new MainPhotoViewPager(imageUrls);
+//		WindowManager wm = (WindowManager) getSystemService("window");
+//		int pageWidth = wm.getDefaultDisplay().getWidth() / 3;
+//		mPager.getLayoutParams().width = pageWidth;
 
 		mPager.setAdapter(adapter);
-		// pager.setAdapter(new MyPagerAdapter());
+		if (imageUrls.size() > 3) {
+			mPager.setCurrentItem(3, false);
+//			WindowManager wm = (WindowManager) getSystemService("window");
+//			mPager.setPageMargin(-1 * wm.getDefaultDisplay().getWidth() / 3);
+			mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+				@Override
+				public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+					Log.d("Log", "------------------------------");
+					Log.d("Log", "onPageScrolled");
+					Log.d("Log", "position = " + position);
+					Log.d("Log", "positionOffset = " + positionOffset);
+					Log.d("Log", "positionOffsetPixels = " + positionOffsetPixels);
+					Log.d("Log", "------------------------------");
+					if (positionOffsetPixels == 0) {
+						if (position == 0) {
+							mPager.setCurrentItem(mPager.getAdapter().getCount() - 3 * 2, false);
+						} else if (position == mPager.getAdapter().getCount() - 1) {
+							mPager.setCurrentItem(3 * 2 - 1, false);
+						}
+					}
+				}
 
-//        ActionBar actionBar = getActionBar();
-//        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-//        for (int i = 0; i < pgaeNum; i++) {
-//            actionBar.addTab(actionBar.newTab().setText(i + "").setTabListener(this));
-//        }
+				@Override
+				public void onPageSelected(int position) {
+					Log.d("Log", "------------------------------");
+					Log.d("Log", "onPageSelected");
+					Log.d("Log", "position = " + position);
+					Log.d("Log", "------------------------------");
+				}
 
-		findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onPageScrollStateChanged(int state) {
+					Log.d("Log", "------------------------------");
+					Log.d("Log", "onPageScrollStateChanged");
+					Log.d("Log", "position = " + state);
+					Log.d("Log", "------------------------------");
+				}
+			});
+		}
 
+		findViewById(R.id.btnLeft).setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-//                adapter.addPage();
-//                adapter.notifyDataSetChanged();
-				mPager.setCurrentItem((mPager.getCurrentItem() - 1 + imageUrls.size()) % imageUrls.size(), true);
+				mPager.setCurrentItem(mPager.getCurrentItem() - 1, true);
 			}
 		});
 
-		findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
-
+		findViewById(R.id.btnRight).setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-//                adapter.removePage();
-//                adapter.notifyDataSetChanged();
-				mPager.setCurrentItem((mPager.getCurrentItem() + 1) % imageUrls.size(), true);
+				mPager.setCurrentItem(mPager.getCurrentItem() + 1, true);
 			}
 		});
 	}
 
-	class MyPagerAdapter extends PagerAdapter {
+	class MainPhotoViewPager extends android.support.v4.view.PagerAdapter {
 
-		public MyPagerAdapter() {
+		// 同時に表示するページ数鵜
+		private static final int DISP_PAGE_COUNT = 3;
+		private int mVirtualCount = 0;
+		private int mRealCount = 0;
+		private List<String> mImageUrls;
+
+		public MainPhotoViewPager(List<String> imageUrls) {
 			super();
+			if (imageUrls == null) {
+				// コンストラクタの引数はすべて必須のため１つでもnullの引数があった場合は例外を発行
+				throw new IllegalArgumentException("MainPhotoViewPager null in the argument");
+			}
+			mImageUrls = imageUrls;
+			mRealCount = mImageUrls.size();
+			if (mRealCount > DISP_PAGE_COUNT) {
+				// 表示ページ数より多い場合
+				mVirtualCount = mRealCount + DISP_PAGE_COUNT * 2;
+			} else {
+				// 表示ページ数以下の場合
+				mVirtualCount = mRealCount;
+			}
 		}
 
 		@Override
 		public Object instantiateItem(ViewGroup container, int position) {
 			TextView tv = new TextView(container.getContext());
+			WindowManager wm = (WindowManager) getSystemService("window");
+			tv.setWidth(wm.getDefaultDisplay().getWidth() / 3);
+//			tv.setLayoutDirection((int) wm.getDefaultDisplay().getWidth() / 3);
+			tv.setPadding(
+					-wm.getDefaultDisplay().getWidth() / 3,
+					0,
+					-wm.getDefaultDisplay().getWidth() / 3,
+					0);
+			tv.setGravity(Gravity.CENTER);
+			int virtualPosition;
+			if (mVirtualCount == mRealCount) {
+				// ページングなし
+				virtualPosition = position;
+				tv.setBackgroundColor(Color.parseColor("#FFFFFF"));
+			} else {
+				virtualPosition = (position - DISP_PAGE_COUNT + mRealCount) % mRealCount;
+				if (0 <= position && position <= DISP_PAGE_COUNT - 1) {
+					// 左側の仮ページ
+					tv.setBackgroundColor(Color.parseColor("#FF0000"));
+				} else if (mVirtualCount - DISP_PAGE_COUNT <= position && position <= mVirtualCount - 1) {
+					// 左側の仮ページ
+					tv.setBackgroundColor(Color.parseColor("#FFFF00"));
+				} else {
+					// 中央の実ページ
+					tv.setBackgroundColor(Color.parseColor("#FFFFFF"));
+				}
+			}
 			tv.setTextSize(100);
-			tv.setText(position + "");
+			tv.setText(virtualPosition + "");
 			container.addView(tv);
+			Log.d("Log", "------------------------------");
+			Log.d("Log", "DISP_PAGE_COUNT = " + DISP_PAGE_COUNT);
+			Log.d("Log", "mVirtualCount = " + mVirtualCount);
+			Log.d("Log", "mRealCount = " + mRealCount);
+			Log.d("Log", "position = " + position);
+			Log.d("Log", "virtualPosition = " + virtualPosition);
+			Log.d("Log", "------------------------------");
 			return tv;
 		}
 
@@ -95,52 +177,6 @@ public class MainActivity extends Activity {
 
 		@Override
 		public int getCount() {
-			return 5;
-		}
-
-		@Override
-		public boolean isViewFromObject(View view, Object object) {
-			return view == ((View) object);
-		}
-	}
-
-	class MainPhotoViewPager extends android.support.v4.view.PagerAdapter {
-
-		// 同時に表示するページ数鵜
-		private static final int DISP_PAGE_COUNT = 3;
-		private int mVirtualCount = 0;
-		private int mRealCount = 0;
-
-		public MainPhotoViewPager(List<String> imageUrls) {
-			super();
-			if (imageUrls == null) {
-				// コンストラクタの引数はすべて必須のため１つでもnullの引数があった場合は例外を発行
-				throw new IllegalArgumentException("MainPhotoViewPager null in the argument");
-			}
-			if (imageUrls.size() > DISP_PAGE_COUNT) {
-				// 表示ページ数より多い場合
-				mVirtualCount = imageUrls.size() + DISP_PAGE_COUNT * 2;
-			} else {
-				// 表示ページ数以下の場合
-				mVirtualCount = imageUrls.size();
-			}
-			mRealCount = imageUrls.size();
-		}
-
-		@Override
-		public Object instantiateItem(ViewGroup container, int position) {
-//            return super.instantiateItem(container, position);
-			return SimpleFragment.newInstance(position);
-		}
-
-//		@Override
-//		public void destroyItem(ViewGroup container, int position, Object object) {
-//			View v = (View) object;
-//			container.removeView(v);
-//		}
-
-		@Override
-		public int getCount() {
 			return mVirtualCount;
 		}
 
@@ -149,28 +185,6 @@ public class MainActivity extends Activity {
 			return view == object; // false
 		}
 
-	}
-
-	public static class SimpleFragment extends Fragment {
-
-		public static SimpleFragment newInstance(int position) {
-			Bundle args = new Bundle();
-			args.putInt("position", position);
-			SimpleFragment sf = new SimpleFragment();
-			sf.setArguments(args);
-			return sf;
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-			int position = getArguments().getInt("position");
-
-			TextView tv = new TextView(inflater.getContext());
-			tv.setTextSize(100);
-			tv.setText(position + "");
-			return tv;
-		}
 	}
 
 }
